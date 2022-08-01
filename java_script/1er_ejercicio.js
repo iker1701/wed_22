fabab8be7f139711c6c6b5d55dd277d1262a3146
ikerrivas <ikerrivas65@gmail.com>
2022-08-01 11:58:34 -0500

    ejercicico jave  1

---
+++ java_script/1er_ejercicio.js
@@ -0,0 +1,70 @@
+const dic= {
+name:"valeria",
+apellidosp:"juarez",
+apellidosM:"de la paz",
+edad: 76
+}
+console.log(dic.name);
+console.log(dic.apellidosp);
+console.log(dic);
+dic.edad=27;
+console.log(dic.edad);
+console.log(dic);
+
+var iñaki={
+	comoes:"negro"
+}
+console.log(iñaki.comoes)
+
+var nombre="valeria";
+var apellido="jimeno";
+
+console.log("hola name llamo:"+nombre + " "+ apellido);
+
+console.log(`hola me llamo :${nombre} ${apellido}`);
+
+var a =10;
+var b =20;
+
+console.log(`la suma de a + b es: ${a+b}`);
+console.log(-7/0); 
+console.log("hola"/2);
+
+
+var t = true;
+var f =false;
+
+
+var n = null;
+console.log(n);
+
+var conversion = '123456'
+// console.log(conversion);
+// console.log(typeo(conversion));
+
+var conversion = 123456
+// console.log(conversion);
+// console.log(typeo(conversion));
+
+console.log(`tipo de dato de la variable t: ${typeof(t)}`)
+
+var n ="30";
+console.log(typeof(n));
+
+
+n = Number(n);
+console.log(n);
+
+console.log(typeof(+'123'));
+
+t = Number(t)
+f = Number(f)
+
+console.log(t)
+console.log(f)
+
+console.log(t);
+console.log(typeof(t));
+
+console.log(f);
+console.log(typeof(f));
