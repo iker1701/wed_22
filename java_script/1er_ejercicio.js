fdf101501f89eaec7c648d6d942e3bbb1bf74151
ikerrivas <ikerrivas65@gmail.com>
2022-08-02 11:50:56 -0500

    2 clase java script

--- java_script/1er_ejercicio.js
+++ java_script/1er_ejercicio.js
@@ -1,70 +1,105 @@
-const dic= {
-name:"valeria",
-apellidosp:"juarez",
-apellidosM:"de la paz",
-edad: 76
-}
-console.log(dic.name);
-console.log(dic.apellidosp);
-console.log(dic);
-dic.edad=27;
-console.log(dic.edad);
-console.log(dic);
+// const dic= {
+// name:"valeria",
+// apellidosp:"juarez",
+// apellidosM:"de la paz",
+// edad: 76
+// }
+// console.log(dic.name);
+// console.log(dic.apellidosp);
+// console.log(dic);
+// dic.edad=27;
+// console.log(dic.edad);
+// console.log(dic);
 
-var iñaki={
-	comoes:"negro"
-}
-console.log(iñaki.comoes)
+// var iñaki={
+// 	comoes:"negro"
+// }
+// console.log(iñaki.comoes)
 
-var nombre="valeria";
-var apellido="jimeno";
+// var nombre="valeria";
+// var apellido="jimeno";
 
-console.log("hola name llamo:"+nombre + " "+ apellido);
+// console.log("hola name llamo:"+nombre + " "+ apellido);
 
-console.log(`hola me llamo :${nombre} ${apellido}`);
+// console.log(`hola me llamo :${nombre} ${apellido}`);
 
-var a =10;
-var b =20;
+// var a =10;
+// var b =20;
 
-console.log(`la suma de a + b es: ${a+b}`);
-console.log(-7/0); 
-console.log("hola"/2);
-
-
-var t = true;
-var f =false;
+// console.log(`la suma de a + b es: ${a+b}`);
+// console.log(-7/0); 
+// console.log("hola"/2);
 
 
-var n = null;
-console.log(n);
-
-var conversion = '123456'
-// console.log(conversion);
-// console.log(typeo(conversion));
-
-var conversion = 123456
-// console.log(conversion);
-// console.log(typeo(conversion));
-
-console.log(`tipo de dato de la variable t: ${typeof(t)}`)
-
-var n ="30";
-console.log(typeof(n));
+// var t = true;
+// var f =false;
 
 
-n = Number(n);
-console.log(n);
+// var n = null;
+// console.log(n);
 
-console.log(typeof(+'123'));
+// var conversion = '123456'
+// // console.log(conversion);
+// // console.log(typeo(conversion));
 
-t = Number(t)
-f = Number(f)
+// var conversion = 123456
+// // console.log(conversion);
+// // console.log(typeo(conversion));
 
-console.log(t)
-console.log(f)
+// console.log(`tipo de dato de la variable t: ${typeof(t)}`)
 
-console.log(t);
-console.log(typeof(t));
+// var n ="30";
+// console.log(typeof(n));
 
-console.log(f);
-console.log(typeof(f));
+
+// n = Number(n);
+// console.log(n);
+
+// console.log(typeof(+'123'));
+
+// t = Number(t)
+// f = Number(f)
+
+// console.log(t)
+// console.log(f)
+
+// console.log(t);
+// console.log(typeof(t));
+
+// console.log(f);
+// console.log(typeof(f));
+
+var uno =1;
+var cero =0;
+
+console.log(`el tipo de dato de la variable uno despues de la conversion aboleando es: ${typeof(Boolean(cero))} y su valor es${Boolean(cero)}.`);
+
+
+var a =5;
+var b =10;
+var c =15;
+
+// console.log(a+b);
+// console.log(b-a);
+// console.log(a*c);
+// console.log(a/b);
+// console.log(èl reciduo de dividir 10/5 es ${b%a}`);
+
+// a++
+// c++
+// b--
+
+// console.log(a);
+// console.log(c);
+// console.log(b);
+
+// var a =5;
+// var b =10;
+// var c =15;
+
+
+var r1 = ((3*a)/c)+a+b+c;
+var r2 = a-(b+c)*a/1;
+
+console.log(r1);
+console.log(r2);
