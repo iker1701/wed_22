b39143226d53671189648bd0f4bb652c0ef1c8da
ikerrivas <ikerrivas65@gmail.com>
2022-08-04 10:29:02 -0500

    clase_3

---
+++ java_script/3er_ejercicio.js
@@ -0,0 +1,25 @@
+// for(let i =1; i <=2000; i++){
+// 	console.log("hola brooooooo");
+// }
+
+
+// var correr =true
+
+// for (let i = 0 ; i <= 200; i++){
+// }
+
+
+// for(let i =2; i <=2000; i--){
+// 	if(i % 2 == 0){
+// 		console.log(i)}
+// 	}
+
+// for(let i =2; i <=2000; i++){
+// 	if(i %  5== 0){
+// 		console.log(i)}
+// 	}
+
+// for(let i =1; i <=100; i++){
+// 	if(i %  10== 0){
+// 		console.log(i)}
+// 	}
